deea8d73361a116926dcb802a374762669cae6ce
TechOps29 <weisnid8@gmail.com>
2025-01-03 19:22:45 +0100

    Update INF_GEM_AND_SPEED.lua
    Added Open Crystal by pressing Q

--- INF_GEM_AND_SPEED.lua
+++ INF_GEM_AND_SPEED.lua
@@ -1,9 +1,29 @@
+local UserInputService = game:GetService("UserInputService")
+local RunService = game:GetService("RunService")
+
+-- Function to handle the pressing of the Q key
+local function onKeyPress(input)
+    if input.KeyCode == Enum.KeyCode.Q then
+        -- Pets action: Open the Electro Legends Crystal
+        local args = {
+            [1] = "openCrystal",
+            [2] = "Electro Legends Crystal"
+        }
+
+        game:GetService("ReplicatedStorage").rEvents.openCrystalRemote:InvokeServer(unpack(args))
+    end
+end
+
+-- Connect to UserInputService to listen for the Q key press
+UserInputService.InputBegan:Connect(onKeyPress)
+
+-- Loop for Gems and Speed actions with the shortest cooldown
 while true do
     -- Gems
     local args1 = {
         [1] = "collectOrb",
         [2] = "Gem",
-        [3] = "City"
+        [3] = "Legends Highway"
     }
 
     game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args1))
@@ -12,7 +32,7 @@ while true do
     local args2 = {
         [1] = "collectOrb",
         [2] = "Orange Orb",
-        [3] = "City"
+        [3] = "Legends Highway"
     }
 
     game:GetService("ReplicatedStorage").rEvents.orbEvent:FireServer(unpack(args2))
